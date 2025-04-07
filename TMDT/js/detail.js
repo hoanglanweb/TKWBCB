e1c0397defd7d80dabadc81903e6cf7fd0a73a8a
NguyenLannnFE <nguyenhoanglan0311@gmail.com>
2025-04-07 09:42:42 +0700

    thay doi

---
+++ TMDT/js/detail.js
@@ -0,0 +1,34 @@
+// hiện thị thôn tin sản phẩm ///
+const urlParams = new URLSearchParams(window.location.search);
+const productIdRaw = urlParams.get("id");
+const productId = productIdRaw ? parseInt(productIdRaw) : null;
+
+console.log("Product ID:", productId);
+
+fetch("/TMDT/Data/data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    const product = data.Nike.find((item) => item.id === productId);
+
+    const container = document.getElementById("product-detail");
+    container.innerHTML = "";
+
+    if (!product) {
+      document.getElementById("product-detail").innerHTML =
+        "<p>San pham khong ton tai!</p>";
+      return;
+    }
+
+    const template = document.getElementById("product-template");
+    
+    // cloneNode là hàm sao chép một node html , true là sao chép đệ quy bên trong 
+    // hàm clone sẽ lấy nội dung bên trong template --> sao chép mọi thứ sâu bên trong --> gán biến vào clone.
+    // dùng appendchild để hiển thị lên trang html
+    
+    const clone = template.content.cloneNode(true); 
+
+    clone.querySelector(".image").src = product.thumbail;
+
+    container.appendChild(clone);
+  })
+  .catch((err) => console.error("loi roi", err));
--- TMDT/js/index.js
+++ TMDT/js/index.js
@@ -3,12 +3,18 @@ let sliderIndex = 0;
 function moveSlide(n) {
   const slides = document.querySelectorAll(".slide"); // Lấy tất cả các slide
   const sliderContainer = document.querySelector(".col-left_image-slider"); // Lấy phần tử chứa slides
-  
+
   // Cập nhật sliderIndex với số bước di chuyển, đảm bảo không vượt quá phạm vi số slide
   sliderIndex = (sliderIndex + n + slides.length) % slides.length;
-  
+
   const offset = -sliderIndex * 100; // Mỗi slide chiếm 100% chiều rộng
 
   sliderContainer.style.transform = `translateX(${offset}%)`;
 }
-setInterval(() => moveSlide(1),3000)
+setInterval(() => moveSlide(1), 3000);
+
+const productContainer = document.getElementById("product");
+
+
+
+
